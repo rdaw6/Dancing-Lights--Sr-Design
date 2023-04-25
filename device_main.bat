8c9dc6e2ffc8208e3319da1d8dee3ac880cfb4f2
rdaw6 <113135441+rdaw6@users.noreply.github.com>
2023-04-24 20:51:15 -0700

    Added Working Files
    Files in Working folder need paths changed to run. Files in Macros and UI run properly from device_main.bat.

---
+++ device_main.bat
@@ -0,0 +1,4 @@
+start python C:\Users\LattePanda\Desktop\Dancing-Lights--Sr-Design-main\UI\main.py > output1.txt
+if errorlevel 1 pause
+start python C:\Users\LattePanda\Desktop\Dancing-Lights--Sr-Design-main\Macros\macros.py
+cmd /k
